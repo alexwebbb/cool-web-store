a53caf9038dc7c8d083ea4c9a1b7305433393985
Alex Webb <alexwebbb@protonmail.com>
2018-01-05 02:39:50 -0600

    stripe works

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+
+stripe.charges.retrieve("ch_1Bgprn2eZvKYlo2CfqGNWziM").then(function(charge) {
+	console.log(charge);
+});
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
