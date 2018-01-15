3102fc451d625c2af70aa26c21db08a373072c5f
Alex Webb <alexwebbb@protonmail.com>
2018-01-15 13:07:02 -0600

    converted to express generator

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -5,3 +5,7 @@ $("#js-main").click(() => {
 	$("#js-aside-pre").toggleClass("col-md-1 col-md-4");
 	$("#js-main").toggleClass("col-md-12 col-md-8 offset-md-4");
 });
+
+const stripe = Stripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh"),
+	elements = stripe.elements(),
+	cardElement = elements.create("card");
