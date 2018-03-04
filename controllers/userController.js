c59336a6f3aaf7fac2e3e2153b621dad80ef2294
Alex Webb <alexwebbb@protonmail.com>
2018-03-03 23:15:27 -0600

    fixed user create bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -135,45 +135,38 @@ exports.user_create_post = [
 	...validateAndSanitizeFields,
 	// Process request after validation and sanitization.
 	(req, res, next) => {
-		if (
-			req.user._id.equals(req.params.id) ||
-			req.user.user_group === "admin"
-		) {
-			// Extract the validation errors from a request.
-			const errors = validationResult(req);
-			errors.array().push({ msg: "testing 123" });
-			if (!errors.isEmpty()) {
-				// There are errors. Render form again with sanitized values/errors messages.
-				res.render("user/form", {
-					title: "Create User",
-					user: req.body,
-					errors: errors.array()
-				});
-				return;
-			} else {
-				salt(req.body.password).hash(function(err, hash) {
-					// Create a user object with escaped and trimmed data.
-					const user = new User({
-						username: req.body.username,
-						hashedPassword: hash,
-						email: req.body.email,
-						first_name: req.body.first_name,
-						middle_name: req.body.middle_name,
-						last_name: req.body.last_name,
-						user_group: req.body.admin ? "admin" : "user"
-					});
-
-					user.save(function(err) {
-						if (err) {
-							return next(err);
-						}
-						// Successful - redirect to new user record.
-						res.redirect(user.url);
-					});
-				});
-			}
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		errors.array().push({ msg: "testing 123" });
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render("user/form", {
+				title: "Create User",
+				user: req.body,
+				errors: errors.array()
+			});
+			return;
 		} else {
-			res.redirect("/login");
+			salt(req.body.password).hash(function(err, hash) {
+				// Create a user object with escaped and trimmed data.
+				const user = new User({
+					username: req.body.username,
+					hashedPassword: hash,
+					email: req.body.email,
+					first_name: req.body.first_name,
+					middle_name: req.body.middle_name,
+					last_name: req.body.last_name,
+					user_group: req.body.admin ? "admin" : "user"
+				});
+
+				user.save(function(err) {
+					if (err) {
+						return next(err);
+					}
+					// Successful - redirect to new user record.
+					res.redirect(user.url);
+				});
+			});
 		}
 	}
 ];
